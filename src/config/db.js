2126124aaedffa6d389f24f74be48ef96e0d4c89
Sivaraj M <sivaraj2siva@gmail.com>
2026-06-10 19:38:08 +0530

    fix: db connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   // mongoose.connect() manages the connection pool internally.
   // No need for a manual client singleton like the native driver.
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(process.env.MONGODB_URI, {
+    dbName: process.env.PRIMARY_DB_NAME,
+  });
 
-  console.log(`Mongoose connected: ${mongoose.connection.host}`);
+  console.log(
+    `Mongoose connected: ${mongoose.connection.host}`,
+    mongoose.connection,
+  );
 };
